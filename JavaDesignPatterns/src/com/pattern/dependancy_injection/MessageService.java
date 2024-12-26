f9464e5d9b01bce2afbbfe33be7000082b7cecf4
DESKTOP-BLIA7FJ\Sourabh <sourabh.teke@gmail.com>
2024-12-26 15:04:55 +0530

    Delete old tutorials

--- JavaDesignPatterns/src/com/pattern/dependancy_injection/MessageService.java
+++
@@ -1,5 +0,0 @@
-package com.pattern.dependancy_injection;
-
-public interface MessageService {
-	void sendMessage(String msg, String rec);
-}
--- JavaDesignPatterns/src/com/pattern/dependancy_injection/MessageServiceInjector.java
+++
@@ -1,5 +0,0 @@
-package com.pattern.dependancy_injection;
-
-public interface MessageServiceInjector {
-	public ConsumerApplication getConsumerApplication();
-}
