fb161ec5854877a2fde057a8a924f8614dbf1ce9
DESKTOP-BLIA7FJ\Sourabh <sourabh.teke@gmail.com>
2024-12-29 14:06:53 +0530

    Adds VersionController for providing end point to check git branch and commit id details for build

---
+++ SpringBootTutorials/spring-micrometer-prometheus-graphana/src/main/java/com/maxlogic/tutorial/spring_micrometer_prometheus_graphana/controller/VersionController.java
@@ -0,0 +1,43 @@
+package com.maxlogic.tutorial.spring_micrometer_prometheus_graphana.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/version")
+public class VersionController {
+
+    @GetMapping()
+    public String versionInformation() {
+        return readGitProperties();
+    }
+
+    private String readGitProperties() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("git.properties");
+        try {
+            return readFromInputStream(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Version information could not be retrieved";
+        }
+    }
+
+    private String readFromInputStream(InputStream inputStream)
+            throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
+    }
+
+}
